844e4abbb21621825ec7bc801ac49af043465e42
kyubiiash <47471848+kyubiiash@users.noreply.github.com>
2019-02-11 15:20:53 -0500

    Update stats project 1.R

--- stats project 1.R
+++ stats project 1.R
@@ -1,5 +1,5 @@
 library(readr)
 library(dplyr)
-NCAA_data = read.table("~/Desktop/Stats Project 1/basketball_use.dat")
+NCAA_data = read.table("https://daviddalpiaz.github.io/stat3202-sp19/project/proj-01/data/basketball_use.dat")
 names(NCAA_data) = c("year", "school_home", "score_home", "school_away", "score_away")
-NCAA_data$newcolumn = NCAA_data$score_home + NCAA_data$score_away
+NCAA_data$total _score= NCAA_data$score_home + NCAA_data$score_away
